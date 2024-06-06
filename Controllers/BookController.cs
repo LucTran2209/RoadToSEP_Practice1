4e374b14d6716966b895d1d7e8af93d49f206397
minmetmoi <minh1182002@gmail.com>
2024-06-07 00:26:53 +0700

    bookcontroller commit

---
+++ Controllers/BookController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practice1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+
+    }
+}
